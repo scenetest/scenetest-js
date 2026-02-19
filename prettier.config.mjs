294f5cc22282815f163f7179e197b546c3cad83d
Em Snook <me@msnook.xyz>
2026-02-19 13:56:06 +0530

    Add prettier and release script

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  useTabs: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  experimentalTernaries: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config
